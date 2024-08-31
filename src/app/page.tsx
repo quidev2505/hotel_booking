241186d651791140484c030d41c62c1fa091edda
--replace-all <quidev2505@gmail.com>
2024-08-31 15:11:59 +0700

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-import HomePage from "./pages/page";
+"use client";
+import React, { cache, Suspense } from "react";
+import Loading from "./loading";
+import RoomList from "@/components/Roomlists";
+const fetchRooms = cache(async () => {
+  try {
+    const response = await fetch("/api/rooms/");
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-export default function Home() {
-  return <HomePage />;
-}
+const HomePage: React.FC = async () => {
+  const rooms = await fetchRooms();
+
+  if (!rooms) return <div>Loading...</div>;
+
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <div className="container mx-auto px-4 py-8">
+          <RoomList rooms={rooms} />
+        </div>
+      </Suspense>
+    </>
+  );
+};
+
+export default HomePage;
